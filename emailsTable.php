322b646914d68b29b160c3d397cf8a8f3d9b5134
SaadBarhrouj <saad.barhrouj@etu.uae.ac.ma>
2025-02-06 13:58:46 +0100

    Ajout des interfaces emailsTable et ajouterEmail

---
+++ emailsTable.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Liste des Emails</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <section class="cards">
+    <a href="index.php" class="return-button" ><i class="fa fa-arrow-left"></i> Retour</a>
+        <h1 class="title" >Liste des Emails</h1>
+        <table >
+            <thead>
+                <tr>
+                    <th>Adresse Email</th>
+                    <th>Fréquence</th>
+                </tr>
+            </thead>
+            <tbody>
+
+         <!-- a supprime  apres backend -->
+                  <tr>
+                    <td>saad@gmail.com</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <td>hiba@gmail.com</td>
+                    <td>15</td>
+                </tr>
+                <tr>
+                    <td>nada@gmail.com</td>
+                    <td>20</td>
+                </tr>
+         <!-- a supprime  apres backend -->
+            </tbody>
+        </table>
+    </section>
+   
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Gestion des Emails</title>
+</head>  
+
+<body>
+ 
+    <section class="cards" >
+        <h2 class="title">Gestion des Emails</h2>
+        <div class="content">
+
+        <div class="card">
+        <div class="icon">
+            <a href="ajouterEmail.php"><i class="fa fa-plus"></i></a>
+        </div>
+        <div class="info">
+            <h3>Ajouter un Email</h3>
+            <p>Ajouter une nouvelle adresse email à la liste des emails enregistrés.</p>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="icon">
+           <a href="emailsTable.php"> <i class="fa fa-table"></i></a>
+        </div>
+        <div class="info">
+            <h3>Afficher les Emails</h3>
+            <p>Voir toutes les adresses emails enregistrées et leur fréquence d'utilisation.</p>
+        </div>
+    </div>    
+
+        </div>
+    </section>
+
+
+</body>
+
+</html>
