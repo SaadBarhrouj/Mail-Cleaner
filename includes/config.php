15f710541b7256fc4c65acac6bc8291a0a6aef28
SaadBarhrouj <saad.barhrouj@etu.uae.ac.ma>
2025-02-11 14:27:45 +0100

    update

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,7 @@
 <?php
 define('valid_file', '../data/Emails.txt');
 define('emailsT_file', '../data/EmailsT.txt');
+define('verified_file', '../data/verified_emails.txt');
 define('invalid_file', '../data/AdressesEmailsNonValides.txt');
 define('domain_folder', '../data/domains/');
 ?>
--- templates/emailsVerifieTable.php
+++ templates/emailsVerifieTable.php
@@ -1,59 +1,75 @@
-<?php
-include '../includes/config.php';
+<?php 
 include '../includes/functions.php';
+include '../includes/config.php';  
 
-$emails = lire_emails(valid_file);
-
-$emails_valides = $emails['valides'];
-$emails_non_valides = $emails['non_valides'];
-
-foreach (array_keys($emails_non_valides) as $email) {
-    if (!email_non_valide_exist($email, invalid_file)) {
-        file_put_contents(invalid_file, $email . PHP_EOL, FILE_APPEND);
-    }
+function validateEmail($email) {
+    $pattern = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+    return preg_match($pattern, $email);
 }
 
-$emails_uniques_valides = array_keys($emails_valides);
-$emails_uniques_valides = array_unique($emails_uniques_valides);
 
-sort($emails_uniques_valides);
+function lire_emails($fichier) {
+    $emails_valides = [];
+    $emails_non_valides = [];
 
-file_put_contents(emailsT_file, implode(PHP_EOL, $emails_uniques_valides));
+    if (file_exists($fichier)) {
+        $lines = file($fichier, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            $line_emails = preg_split('/\s+|,|;/', $line);
+            foreach ($line_emails as $email) {
+                if (validateEmail($email)) {
+                    $emails_valides[] = $email;
+                } else {
+                    $emails_non_valides[] = $email;
+                }
+            }
+        }
+        $emails_valides_string = implode(PHP_EOL, $emails_valides);
+        file_put_contents($fichier, $emails_valides_string);
+    }
+
+    return [
+        'valides' => array_count_values($emails_valides),
+        'non_valides' => array_count_values($emails_non_valides)
+    ];
+}
+
+function email_verify($file,$file_verified_emails){
+    $emails_verifies = [];
+    $emails = lire_emails($file);
+    $emails_valides = array_keys($emails['valides']);
+
+    foreach($emails_valides as $email){
+        // Initialize cURL.
+        $ch = curl_init();
+        $api_key="d04618536639472e9c8dce61713a7780";
+
+        // Set the URL that you want to GET by using the CURLOPT_URL option.
+        curl_setopt($ch, CURLOPT_URL, "https://emailvalidation.abstractapi.com/v1/?api_key=$api_key&email=$email");
+
+        // Set CURLOPT_RETURNTRANSFER so that the content is returned as a variable.
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Set CURLOPT_FOLLOWLOCATION to true to follow redirects.
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+
+        // Execute the request.
+        $response= curl_exec($ch);
+
+
+        // Close the cURL handle.
+        curl_close($ch);
+
+        $data =json_decode($response,true);
+
+        // Print the data out onto the page.
+                if($data['deliverability']==="DELIVERABLE"){
+                    file_put_contents($file_verified_emails, "\n" . $email, FILE_APPEND);
+                }
+                
+        }
+}
+email_verify(valid_file,verified_file);
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Liste des Emails Valides</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="../includes/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../includes/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../includes/favicons/favicon-16x16.png">
-    <link rel="manifest" href="../includes/site.webmanifest">
-</head>
-<body>
-    <section class="cards">
-        <a href="../index.php" class="return-button"><i class="fa fa-arrow-left"></i> Retour</a>
-        <h1 class="title">Liste des Emails Valides</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Adresse Email</th>
-                    <th>Fréquence</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($emails_uniques_valides as $email) : ?>
-                <tr>
-                    <td><?php echo $email; ?></td>
-                    <td><?php echo $emails_valides[$email]; ?></td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </section>
-</body>
-</html>
